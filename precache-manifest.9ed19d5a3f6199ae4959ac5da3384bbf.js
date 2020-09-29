72d2ab174f851dacd71e1edd373afb0b2dd84a5f
aschneem <aschneem@gmail.com>
2020-09-28 21:25:58 -0500

    Updates

---
+++ precache-manifest.9ed19d5a3f6199ae4959ac5da3384bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cf5834b502f2098c8854c185d2bb56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf7a56af6efba77ee86",
+    "url": "/static/css/main.60738f47.chunk.css"
+  },
+  {
+    "revision": "f7e8ec95e5ab6bf05a1b",
+    "url": "/static/js/2.3e557dea.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3e557dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf7a56af6efba77ee86",
+    "url": "/static/js/main.36756eed.chunk.js"
+  },
+  {
+    "revision": "164290a73d98cf4f149a",
+    "url": "/static/js/runtime-main.4d5319c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WeddingWebsite/precache-manifest.208702bddc16b3b3ef6dedd83ea39fe5.js"
+  "/precache-manifest.9ed19d5a3f6199ae4959ac5da3384bbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WeddingWebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
